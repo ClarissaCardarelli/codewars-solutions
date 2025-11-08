08a78717f230149b436907fefc1af37c2bac6133
ClarissaCardarelli <ccardare@gmail.com>
2025-11-08 22:12:54 +0100

    finsihed sums

---
+++ arraySumDiff.js
@@ -0,0 +1,28 @@
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0
+
+function sumOfDifferences(arr) {
+  if (arr.length < 2) return 0;
+
+  arr.sort((a, b) => b - a);
+
+  return arr[0] - arr[arr.length - 1];
+}
+
+// function sumOfDifferences(arr) {
+//   if (arr.length < 2) return 0;
+
+//   arr.sort((a, b) => b - a);
+
+//   return arr.reduce((sum, current, index) => {
+//     if (index === arr.length - 1) return sum;
+//     return sum + (current - arr[index + 1]);
+//   }, 0);
+// }
